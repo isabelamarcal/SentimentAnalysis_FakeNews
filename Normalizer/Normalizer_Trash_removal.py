276344e0cc68d80eba08261b29127d2728892669
Wesley Barbosa <wesleysrs_95@hotmail.com>
2018-08-11 10:02:00 -0300

    Inclusão da Normalização e Tokenização do dataset

---
+++ Normalizer/Normalizer_Trash_removal.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Normalização de textos para Processamento de Linguagem Natural
+TRABALHO DE CONCLUSÃO DE CURSO - 2 sem. 2018.
+"Análise da influência de Atributos de Sentimento na Identificação de Fake News"
+"""
+import json
+import re
+import unicodedata
+import tkinter as tk
+from tkinter import filedialog
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+nltk.download('stopwords')
+
+file_opt={}
+
+#Criação do arquivo Json filtrado e normalizado
+def create_normalized_Json(title,author,info,url):
+    
+    name_arq = filedialog.asksaveasfilename(**file_opt)
+    arq = open(name_arq,'w')
+    data = {'title':title,'author':author,'information':info,'url':url}
+    json.dump(data,arq) 
+    arq.close()
+    return 
+
+#Remoção de caracteres não alfanuméricos
+def stripNonAlphaNum(information):
+
+    # Unicode normalize transforma um caracter em seu equivalente em latin.
+    nfkd = unicodedata.normalize('NFKD', information)
+    stripedWord = u"".join([c for c in nfkd if not unicodedata.combining(c)])
+    # Usa expressão regular para retornar a palavra apenas com números, letras e espaço
+    return re.sub('[^a-zA-Z0-9 \\\]', '', stripedWord)
+
+#Normaliza o campo "Description" do arquivo, retirando maiúsculas 
+def normalization(information):
+    
+    return information.lower()
+
+#Faz a leitura do Json no padrão do Dataset "https://github.com/KaiDMML/FakeNewsNet"
+def read_Json():
+    
+    root = tk.Tk()
+    root.withdraw()
+
+    inputPath = filedialog.askopenfilename()
+    data_json = open(inputPath).read()
+    data_jsonInfo = json.loads(data_json)
+    
+    title = data_jsonInfo.get('meta_data').get('og').get('title')
+    author = data_jsonInfo.get('authors')
+    author = str(author).replace('[','')
+    author = str(author).replace(']','')
+    info = data_jsonInfo.get('meta_data').get('og').get('description')
+    url = data_jsonInfo.get('meta_data').get('og').get('url')
+    
+    info = normalization(info)
+    info = stripNonAlphaNum(info)    
+    create_normalized_Json(title,author,info,url)
+    
+    print('Title: ',title)
+    print('Author name: ',author)
+    print('Information: ',info)
+    print('Url: ',url)
+
+"""
+Informations:
+    authors
+    meta-data/og/title
+    meta-data/og/description     <- Normalize!
+    meta-data/og/url
+"""
+
+"""
+{‘ourselves’, ‘hers’, ‘between’, ‘yourself’, ‘but’, ‘again’, ‘there’, ‘about’, 
+‘once’, ‘during’, ‘out’, ‘very’, ‘having’, ‘with’, ‘they’, ‘own’, ‘an’, ‘be’, 
+‘some’, ‘for’, ‘do’, ‘its’, ‘yours’, ‘such’, ‘into’, ‘of’, ‘most’, ‘itself’, 
+‘other’, ‘off’, ‘is’, ‘s’, ‘am’, ‘or’, ‘who’, ‘as’, ‘from’, ‘him’, ‘each’, 
+‘the’, ‘themselves’, ‘until’, ‘below’, ‘are’, ‘we’, ‘these’, ‘your’, ‘his’, 
+‘through’, ‘don’, ‘nor’, ‘me’, ‘were’, ‘her’, ‘more’, ‘himself’, ‘this’, ‘down’
+, ‘should’, ‘our’, ‘their’, ‘while’, ‘above’, ‘both’, ‘up’, ‘to’, ‘ours’, 
+‘had’, ‘she’, ‘all’, ‘no’, ‘when’, ‘at’, ‘any’, ‘before’, ‘them’, ‘same’, 
+‘and’, ‘been’, ‘have’, ‘in’, ‘will’, ‘on’, ‘does’, ‘yourselves’, ‘then’, ‘that’
+, ‘because’, ‘what’, ‘over’, ‘why’, ‘so’, ‘can’, ‘did’, ‘not’, ‘now’, ‘under’, 
+‘he’, ‘you’, ‘herself’, ‘has’, ‘just’, ‘where’, ‘too’, ‘only’, ‘myself’, 
+‘which’, ‘those’, ‘i’, ‘after’, ‘few’, ‘whom’, ‘t’, ‘being’, ‘if’, ‘theirs’, 
+‘my’, ‘against’, ‘a’, ‘by’, ‘doing’, ‘it’, ‘how’, ‘further’, ‘was’, ‘here’, 
+‘than’}
+"""
+
+# lemos o JSON em disco
+#Main function
+
+read_Json()
+    
+        
+        
