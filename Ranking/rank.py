76407923b73faab3d9f736425bdc2490cfb57231
isabela <isabelasilva@gec.inatel.br>
2018-08-15 13:24:05 -0300

    stemming

--- Ranking/rank.py
+++ Ranking/rank.py
@@ -1,32 +1,24 @@
-from nltk.tokenize import word_tokenize
 import nltk
-from nltk.metrics.spearman import *
-from nltk.collocations import *
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import SnowballStemmer
+from nltk.tokenize import PunktSentenceTokenizer
+
 nltk.download('punkt')
-#from tokenize import tokenize, untokenize, NUMBER, STRING, NAME, OP
+nltk.download('averaged_perceptron_tagger')
 
 
 def freqDist(text):
-    # text = "claims democratic candidate " \
-    #        " governor matt  turned flynn told one child predator " \
-    #        " wait see turned authorities"
-    #
+    sentences = nltk.sent_tokenize(text)
+    for sent in sentences:
+        print(nltk.pos_tag(nltk.word_tokenize(sent)))
+    ps = SnowballStemmer("english")
+
     tokens = word_tokenize(text)
+    tokens[:] = [ps.stem(word) for word in tokens]
+
     freq = nltk.FreqDist(tokens)
-    #sort_dict = sorted(freq, key=lambda x: (-freq[x], x))
     return freq
-# #não - direcionado
-# print('Não-Direcionado:')
-# for key in sortDict:
-#     print(key, freq[key])
 
-# #comprimento medio palavras
-#
-# print('Cumprimento medio das palavras:')
-# mediumLen = text.replace(" ","").__len__()/tokens.__len__()
-# print(mediumLen)
-
-  #  freq.plot(5)
 
 
 
