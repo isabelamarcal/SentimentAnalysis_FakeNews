bca2bb17d92a75e3c6d6cc1ac00133d285c027b8
williamtheblack <williampinheiro@gec.inatel.br>
2018-08-20 22:09:41 -0300

    Implementação da Análise de URLs

--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 import Normalizer.Normalizer_Trash_removal as normalizer
 import Ranking.rank as rank
+import Source_Analyzes.analize as aSource
 
 from tkinter import filedialog
 
@@ -9,6 +10,10 @@ from tkinter import filedialog
 inputPath = filedialog.askopenfilename()
 data_json = open(inputPath).read()
 
+# analisa a fonte
+aSource.analizesSource(data_json)
+
+#normaliza - continuação
 info = normalizer.normalization(data_json)
 info = normalizer.stripNonAlphaNum(info)
 info_tokenized = normalizer.tokenize_info(info)
